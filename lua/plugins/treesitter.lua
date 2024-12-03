6009afa96d2bc971eaef367b02543092fc8701e0
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2024-12-03 14:13:19 +0100

    Treesitter/autoinstall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "rust" },
+            auto_install = true,
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
